fb9301c3344ad62f8bc28670a167601fbd7342cb
tsu-nera <fox10225fox@gmail.com>
2015-05-02 03:01:45 +0900

    記事おわりにアドセンス追加

---
+++ src/mymodule/double_rectangle_ad.php
@@ -0,0 +1,35 @@
+<p style=”font-size:5pt; color:#999999″><center>スポンサードリンク</center></p>
+<div id="doublerectangle">
+  <table cellspacing="15">
+    <tr>
+      <td>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:336px;height:280px"
+             data-ad-client="ca-pub-8186728334774578"
+             data-ad-slot="4333590219"></ins>
+        <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+      </td>
+      <td>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        <ins class="adsbygoogle"
+             style="display:inline-block;width:336px;height:280px"
+             data-ad-client="ca-pub-8186728334774578"
+             data-ad-slot="1018997016"></ins>
+        <script>
+         (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>  
+      </td>
+    </tr>
+  </table>
+</div>
+
+<style type="text/css">
+ #doublerectangle table {
+   width: auto;
+   margin-left: auto;
+   margin-right: auto;           
+ }
+</style>
