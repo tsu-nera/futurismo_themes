acf63e433d28f73122fed9b725b70468ae41f624
tsu-nera <fox10225fox@gmail.com>
2015-05-02 21:37:43 +0900

    アドセンス追加

---
+++ src/mymodule/ad-in-body.php
@@ -0,0 +1,25 @@
+<p style=”font-size:5pt; color:#999999″><center>スポンサードリンク</center></p>
+<div id="singlerectangle">
+  <table cellspacing="15">
+  <tr>
+    <td>
+      <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      <ins class="adsbygoogle"
+           style="display:inline-block;width:336px;height:280px"
+           data-ad-client="ca-pub-8186728334774578"
+           data-ad-slot="7821780212"></ins>
+      <script>
+       (adsbygoogle = window.adsbygoogle || []).push({});
+      </script>
+    </td>
+  </tr>
+  </table>
+</div>
+
+<style type="text/css">
+ #singlerectangle table {
+   width: auto;
+   margin-left: auto;
+   margin-right: auto;           
+ }
+</style>
--- src/mymodule/double_rectangle_ad.php
+++ src/mymodule/double_rectangle_ad.php
@@ -33,3 +33,4 @@
    margin-right: auto;           
  }
 </style>
+
